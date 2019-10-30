ad81af6367d7f41d9b8cb5578ad90ea00004d6e4
James Min <jmin499@gmail.com>
2019-10-30 20:34:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,15 @@ class Song
   end
   
   def self.new_by_name(name)
-    # binding.pry
     song = self.new
     song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
     song
   end
 
