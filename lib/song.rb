ea38601d09c55a4a1576f6b4c152570d8d3550ea
James Min <jmin499@gmail.com>
2019-10-30 21:35:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,7 +63,12 @@ class Song
   end
   
   def self.create_from_filename(name)
-    
+    splitted = name.split(/\s+-\s+|.mp3/)
+    song = Song.new
+    song.artist_name = splitted[0]
+    song.name = splitted[1]
+    song.save
+    song
   end
 
 end
